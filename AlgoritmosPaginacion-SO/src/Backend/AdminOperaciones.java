6c269d18e7aa4c3d5a9964f9ad69a3b55de8027d
viviana8656 <75183880+viviana8656@users.noreply.github.com>
2023-10-22 16:15:29 -0600

    Otro merge ups

--- AlgoritmosPaginacion-SO/src/Backend/AdminOperaciones.java
+++ AlgoritmosPaginacion-SO/src/Backend/AdminOperaciones.java
@@ -91,7 +91,9 @@ public class AdminOperaciones {
         Random random = new Random();
         StringBuilder operacionesString = new StringBuilder();
         
-        ArrayList<Integer> posiblesNew = new ArrayList();
+        ArrayList<Integer> posiblesNew = new ArrayList();        
+        ArrayList<Integer> procesos = new ArrayList();
+
 
         ArrayList<Integer> posiblesKill = new ArrayList();       
       
@@ -99,6 +101,7 @@ public class AdminOperaciones {
         
         //Por cada proceso, todos pueden ser creados ahorita
         //posiblesNew.add(1);
+        procesos.add(1);
         for(int i = 1; i <= numProcesos; i++){
             posiblesNew.add(i);
         }
@@ -119,7 +122,153 @@ public class AdminOperaciones {
             //operacionesString.append(tipo).append("(").append(proceso);
             boolean anoto = false;
             if (tipo.equals("new") && !posiblesNew.isEmpty()) { //Si es new y aun no se ha hecho kill de todos los procesos
-                int proceso = posiblesNew.get(random.nextInt(posiblesNew.size())); //Elijo uno de los posibles procesos
+                int proceso;
+                if(procesos.size() <= numProcesos){
+                    proceso = procesos.size();
+                    procesos.add(procesos.size()+1);
+                    posiblesNew.add(proceso);
+                }else{
+                    proceso = posiblesNew.get(random.nextInt(posiblesNew.size()));
+                } //Elijo uno de los posibles procesos
+                /*if(posiblesNew.size() < numProcesos && random.nextInt(2)==1){
+                    proceso = procesos.get(posiblesNew.size()-1);
+                    proceso++;
+                    posiblesNew.add(proceso);
+                }*/
+                operacionesString.append(tipo).append("(").append(proceso); //Lo anota
+                anoto = true;
+                int tamano = random.nextInt(40); //Elige un tamaño random
+                operacionesString.append(",").append(tamano); //Se mete el tamaño
+                punteros++; //Eso genera un puntero posible de usar
+                posiblesUse.put(punteros,proceso); //Se mete a los posibles Use y lo asocia con el proceso que lo creo
+                if(!posiblesKill.contains(proceso)){
+                    System.out.println("Agrega el proceso " + proceso);
+                    posiblesKill.add(proceso);
+                }
+            }
+            else if(tipo.equals("use") && !posiblesUse.isEmpty()){ //Si hay punteros creados y va a usar uno
+                ArrayList<Integer> punterosActuales = new ArrayList<>(posiblesUse.keySet()); //Obtiene los posibles punteros
+                int puntero = punterosActuales.get(random.nextInt(punterosActuales.size())); // Elige un puntero random a usar
+                operacionesString.append(tipo).append("(").append(puntero); // Lo anota
+                anoto = true;
+            }
+            else if(tipo.equals("delete") && !posiblesUse.isEmpty()){ //Si hay punteros creados y va a borrar uno
+                ArrayList<Integer> punterosActuales = new ArrayList<>(posiblesUse.keySet()); //Obtiene los posibles punteros
+                int puntero = punterosActuales.get(random.nextInt(punterosActuales.size())); // Elige un ptr a borrar
+                operacionesString.append(tipo).append("(").append(puntero); // Lo anota
+                posiblesUse.remove(puntero);
+                anoto = true;
+            }
+            else if(tipo.equals("kill") && 
+                    ! posiblesKill.isEmpty() && killsHechas < killsAntes){
+                int proceso = posiblesKill.remove(random.nextInt(posiblesKill.size())); //Elige un proceso random a eliminar
+                for(int x = 0; x < posiblesNew.size(); x++){
+                    if(posiblesNew.get(x)== proceso){
+                        posiblesNew.remove(x);
+                    }
+                }
+                
+                operacionesString.append(tipo).append("(").append(proceso); //Lo anota
+                Iterator<Map.Entry<Integer, Integer>> iter = posiblesUse.entrySet().iterator();
+                while (iter.hasNext()) {
+                    Map.Entry<Integer, Integer> entry = iter.next();
+                    if (entry.getValue().equals(proceso)) {
+                        iter.remove(); // Elimina el elemento si el valor coincide
+                    }
+                }
+                anoto = true;
+                killsHechas++;
+            }
+            if(anoto){
+                operacionesString.append(")\n");
+            }else{
+                //System.out.println("No entra a nada");
+                i--;
+            }
+        }
+        if(killsHechas == 0){
+            for (int i = 0; i < killsAntes; i++) {
+                int proceso = posiblesKill.remove(random.nextInt(posiblesKill.size())); //Elige un proceso random a eliminar
+                System.out.println("Esto jjd");
+                for(int x = 0; x < posiblesNew.size(); x++){
+                    if(posiblesNew.get(x)== proceso){
+                        System.out.println("Entro aqui kk");
+                        posiblesNew.remove(x);
+                    }
+                }
+
+                operacionesString.append("kill").append("(").append(proceso); //Lo anota
+                Iterator<Map.Entry<Integer, Integer>> iter = posiblesUse.entrySet().iterator();
+                while (iter.hasNext()) {
+                    Map.Entry<Integer, Integer> entry = iter.next();
+                    if (entry.getValue().equals(proceso)) {
+                        iter.remove(); // Elimina el elemento si el valor coincide
+                    }
+                }
+                killsHechas++;
+                operacionesString.append(")\n");
+            }
+        }
+        System.out.println("Asi quedo antes de los after kills: \n" + operacionesString.toString());
+        
+        System.out.println("Procesos faltantes por eliminar : " + posiblesKill);
+        while(!posiblesKill.isEmpty()){
+            System.out.println("Asi van los posibles kills : " + posiblesKill);
+            int proceso = posiblesKill.remove(random.nextInt(posiblesKill.size())); //Elige un proceso random a eliminar
+            System.out.println("Se elimino el proceso de kills " + proceso);
+            killsHechas++;
+            operacionesString.append("kill").append("(").append(proceso); //Lo anota
+            operacionesString.append(")\n");
+        }
+        // Ahora puedes imprimir o usar la cadena generada
+        System.out.println("Asi quedo despues de los after kills: \n" + operacionesString.toString());
+
+        return operacionesString.toString();
+    }
+    
+    public String generarOperaciones(int numProcesos, int numOperaciones, int seed){
+        Random random = new Random(seed);
+        StringBuilder operacionesString = new StringBuilder();
+        
+        ArrayList<Integer> posiblesNew = new ArrayList();        
+        ArrayList<Integer> procesos = new ArrayList();
+
+
+        ArrayList<Integer> posiblesKill = new ArrayList();       
+      
+        HashMap<Integer, Integer> posiblesUse = new HashMap();
+        
+        //Por cada proceso, todos pueden ser creados ahorita
+        //posiblesNew.add(1);
+        procesos.add(1);
+        for(int i = 1; i <= numProcesos; i++){
+            posiblesNew.add(i);
+        }
+
+        String[] tiposInstruccion = {"new", "use", "delete", "kill"};
+        int punteros = 0;
+        
+        int killsAntes = 20*numProcesos/100;
+        
+        int killsDespues = numProcesos - killsAntes;
+        
+        System.out.println(killsAntes);
+        
+        int killsHechas =0;
+        
+        for (int i = 0; i < numOperaciones - killsDespues; i++) {
+            String tipo = tiposInstruccion[random.nextInt(tiposInstruccion.length)];
+            //operacionesString.append(tipo).append("(").append(proceso);
+            boolean anoto = false;
+            if (tipo.equals("new") && !posiblesNew.isEmpty()) { //Si es new y aun no se ha hecho kill de todos los procesos
+                int proceso;
+                if(procesos.size() <= numProcesos){
+                    proceso = procesos.size();
+                    procesos.add(procesos.size()+1);
+                    posiblesNew.add(proceso);
+                }else{
+                    proceso = posiblesNew.get(random.nextInt(posiblesNew.size()));
+                } //Elijo uno de los posibles procesos
                 /*if(posiblesNew.size() < numProcesos && random.nextInt(2)==1){
                     proceso = procesos.get(posiblesNew.size()-1);
                     proceso++;
