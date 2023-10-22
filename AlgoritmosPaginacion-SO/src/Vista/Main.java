ee22be8ec81407ab8efa72dabceee0434b8d2458
viviana8656 <75183880+viviana8656@users.noreply.github.com>
2023-10-22 15:34:05 -0600

    Update Main.java

--- AlgoritmosPaginacion-SO/src/Vista/Main.java
+++ AlgoritmosPaginacion-SO/src/Vista/Main.java
@@ -14,7 +14,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- *
+ *public void showPages(){
+        MemoryManagementUnit mmu = Main.computadora.getMmu();
+        HashMap<String, ArrayList<Pagina>> map = mmu.getMapa();
+        ArrayList<Pagina> paginas = new ArrayList();
+        ArrayList<Integer> ids = new ArrayList();
+        for (int i = 0; i < map.size(); i++) {
+            if(map.get(String.valueOf(i))!= null){
+                paginas.addAll(map.get(String.valueOf(i)));
+                for(int j = 0; j < map.get(String.valueOf(i)).size(); j++){
+                    ids.add(i);
+                }
+            }
+            
+        }
+        System.out.println("YAY6");
+        DefaultTableModel model = (DefaultTableModel) algorithmTable.getModel();
+        System.out.println("YAY7");
+        for(int i = 0; i < paginas.size(); i++){
+            Pagina page = paginas.get(i);
+            model.addRow(new Object[]{page.id, String.valueOf(ids.get(i)),page.direccionFisica, page.direccionVirtual, page.marking});
+            System.out.println("Nice");
+        }
+    }
  * @author richa
  */
 public class Main {
